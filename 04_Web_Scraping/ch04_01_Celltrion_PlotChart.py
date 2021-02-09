81e68b03d7ff86831ef479fc09fd3d5a3d13c22f
OnyxBlack7 <nasisaro@gmail.com>
2021-02-09 15:59:38 +0900

    시세 DB 구축 및 시세 조회 API 개발

--- 04_Web_Scraping/ch04_01_Celltrion_PlotChart.py
+++ 04_Web_Scraping/ch04_01_Celltrion_PlotChart.py
@@ -1,27 +1,59 @@
 import pandas as pd
-from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import requests
 from matplotlib import pyplot as plt
+import mplfinance as mpf
 
-url = 'https://finance.naver.com/item/sise_day.nhn?code=068270&page=1'
+# 4.4.3 맨 뒤 페이지 숫자 구하기
+url = f"http://finance.naver.com/item/sise_day.nhn?code=068270&page=1"
 with urlopen(url) as doc:
-    html = BeautifulSoup(doc, 'lxml') 
-    pgrr = html.find('td', class_='pgRR')
+    html = BeautifulSoup(requests.get(url,
+        headers={"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                               "Chrome/83.0.4103.106 Whale/2.8.108.15 Safari/537.36"}).text, "lxml")
+    pgrr = html.find("td", class_="pgRR")
     s = str(pgrr.a['href']).split('=')
-    last_page = s[-1]  
+    last_page = s[-1]
 
+# 4.4.4 전체 페이지 읽어오기
 df = pd.DataFrame()
-sise_url = 'https://finance.naver.com/item/sise_day.nhn?code=068270'  
+sise_url = 'https://finance.naver.com/item/sise_day.nhn?code=068270'
 for page in range(1, int(last_page)+1):
-    page_url = '{}&page={}'.format(sise_url, page)  
-    df = df.append(pd.read_html(page_url, header=0)[0])
+    page_url = '{}&page={}'.format(sise_url, page)
+    df = df.append(pd.read_html(requests.get(page_url,
+        headers={"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                               "Chrome/83.0.4103.106 Whale/2.8.108.15 Safari/537.36"}).text)[0])
 
+# 차트 출력을 위해 데이터프레임 가공하기
+# df = df.dropna()
+# df = df.iloc[0:30]
+# df = df.sort_values(by='날짜')
+
+# 날짜, 종가 칼럼으로 차트 그리기
+# plt.title('Celltrion (close)')
+# plt.xticks(rotation=45)
+# plt.plot(df['날짜'], df['종가'], 'co-')
+# plt.grid(color='gray', linestyle='--')
+# plt.show()
+
+# 차트 출력을 위해 데이터프레임 가공하기
 df = df.dropna()
-df = df.iloc[0:30] 
-df = df.sort_values(by='날짜') 
+df = df.iloc[0:30]
+df = df.rename(columns={'날짜': 'Date', '시가': 'Open', '고가': 'High', '저가': 'Low', '종가': 'Close', '거래량': 'Volume'})
+df = df.sort_values(by='Date')
+df.index = pd.to_datetime(df.Date)
+df = df[['Open', 'High', 'Low', 'Close', 'Volume']]
 
-plt.title('Celltrion (close)')
-plt.xticks(rotation=45) 
-plt.plot(df['날짜'], df['종가'], 'co-')
-plt.grid(color='gray', linestyle='--')
-plt.show()
+# 엠피엘파이낸스로 캔들 차트 그리기
+# mpf.plot(df, title='Celltrion candle chart', type='candle')
+# mpf.plot(df, title='Celltrion ohlc chart', type='ohlc')
+kwargs = dict(title='Celltrion customized chart', type='candle', mav=(2, 4, 6), volume=True, ylabel='ohlc candles')
+# kwargs: keyword arguments의 약자, mpf.plot() 함수를 호출할 때 쓰이는 여러 인수를 담는 딕셔너리
+
+mc = mpf.make_marketcolors(up='r', down='b', inherit=True)
+# 마켓 색상은 스타일을 지정하는 필수 객체로서, 상승은 빨간색(red)으로 하락은 파란색(blue)으로 지정.
+
+s = mpf.make_mpf_style(marketcolors=mc)
+# 마켓 색상을 인수로 넘겨줘서 스타일 객체를 생성한다.
+
+mpf.plot(df, **kwargs, style=s)
--- 05_Stock_Price_API/Investar/DBUpdaterEx.py
+++ 05_Stock_Price_API/Investar/DBUpdaterEx.py
@@ -9,9 +9,9 @@ from threading import Timer
 class DBUpdater:  
     def __init__(self):
         """생성자: MariaDB 연결 및 종목코드 딕셔너리 생성"""
-        self.conn = pymysql.connect(host='localhost', user='root',
-            password='myPa$$word', db='INVESTAR', charset='utf8')
-        
+        self.conn = pymysql.connect(host='localhost', user='root', password='myPa$$word', db='INVESTAR', charset='utf8')
+        # 한글 회사명 사용하기 때문에 인코딩 오류 발생할 수 있다. 따라서 utf8로 인코딩 형식 미리 지정
+
         with self.conn.cursor() as curs:
             sql = """
             CREATE TABLE IF NOT EXISTS company_info (
@@ -45,33 +45,36 @@ class DBUpdater:
         """KRX로부터 상장기업 목록 파일을 읽어와서 데이터프레임으로 반환"""
         url = 'http://kind.krx.co.kr/corpgeneral/corpList.do?method='\
             'download&searchType=13'
-        krx = pd.read_html(url, header=0)[0]
-        krx = krx[['종목코드', '회사명']]
-        krx = krx.rename(columns={'종목코드': 'code', '회사명': 'company'})
-        krx.code = krx.code.map('{:06d}'.format)
+        krx = pd.read_html(url, header=0)[0]    # 상장법인목록.xls 파일을 read_html()함수로 읽는다.
+        krx = krx[['종목코드', '회사명']]          # 종목코드와 회사명 칼럼만 남긴다.
+        krx = krx.rename(columns={'종목코드': 'code', '회사명': 'company'})    # 한글 칼럼명을 영문으로 변경
+        krx.code = krx.code.map('{:06d}'.format)         # 0이 붙여 6자리로 만든다.
         return krx
     
     def update_comp_info(self):
         """종목코드를 company_info 테이블에 업데이트 한 후 딕셔너리에 저장"""
         sql = "SELECT * FROM company_info"
-        df = pd.read_sql(sql, self.conn)
+        df = pd.read_sql(sql, self.conn)    # company_info 테이블을 read_sql() 함수로 읽는다.
         for idx in range(len(df)):
             self.codes[df['code'].values[idx]] = df['company'].values[idx]
-                    
+            # 위에서 읽은 데이터프레임을 이용해서 종목코드와 회사명으로 codes 딕셔너리를 만든다.
+
         with self.conn.cursor() as curs:
             sql = "SELECT max(last_update) FROM company_info"
             curs.execute(sql)
-            rs = curs.fetchone()
+            rs = curs.fetchone()    # SELECT max() ~ 구문을 이용해서 DB에서 가장 최근 업데이트 날짜를 가져온다
             today = datetime.today().strftime('%Y-%m-%d')
             if rs[0] == None or rs[0].strftime('%Y-%m-%d') < today:
                 krx = self.read_krx_code()
+                # 위에서 구한 날짜가 존재하지 않거나 오늘보다 오래된 경우 KRX 상장기업 목록 파일을 읽어서 krx 데이터프레임에 저장한다.
+
                 for idx in range(len(krx)):
                     code = krx.code.values[idx]
                     company = krx.company.values[idx]                
                     sql = f"REPLACE INTO company_info (code, company, last"\
                         f"_update) VALUES ('{code}', '{company}', '{today}')"
-                    curs.execute(sql)
-                    self.codes[code] = company
+                    curs.execute(sql)   # REPLACE INTO 구문을 이용해서 '종목코드, 회사명, 오늘날짜' 행을 DB에 저장한다.
+                    self.codes[code] = company  # codes 딕셔너리에 키-값으로 종목코드와 회사명을 추가한다.
                     tmnow = datetime.now().strftime('%Y-%m-%d %H:%M')
                     print(f"[{tmnow}] #{idx+1:04d} REPLACE INTO company_info "\
                         f"VALUES ({code}, {company}, {today})")
@@ -83,27 +86,31 @@ class DBUpdater:
         try:
             url = f"http://finance.naver.com/item/sise_day.nhn?code={code}"
             html = BeautifulSoup(requests.get(url,
-                headers={'User-agent': 'Mozilla/5.0'}).text, "lxml")
+                headers={"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                               "Chrome/83.0.4103.106 Whale/2.8.108.15 Safari/537.36"}).text, "lxml")
             pgrr = html.find("td", class_="pgRR")
             if pgrr is None:
                 return None
             s = str(pgrr.a["href"]).split('=')
-            lastpage = s[-1] 
+            lastpage = s[-1] # 마지막 페이지
             df = pd.DataFrame()
             pages = min(int(lastpage), pages_to_fetch)
             for page in range(1, pages + 1):
                 pg_url = '{}&page={}'.format(url, page)
-                df = df.append(pd.read_html(requests.get(pg_url,
-                    headers={'User-agent': 'Mozilla/5.0'}).text)[0])                                          
+                df = df.append(pd.read_html(requests.get(pg_url,    # read_html로 읽어 데이터프레임에 추가
+                    headers={"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                               "Chrome/83.0.4103.106 Whale/2.8.108.15 Safari/537.36"}).text)[0])
                 tmnow = datetime.now().strftime('%Y-%m-%d %H:%M')
                 print('[{}] {} ({}) : {:04d}/{:04d} pages are downloading...'.
                     format(tmnow, company, code, page, pages), end="\r")
             df = df.rename(columns={'날짜':'date','종가':'close','전일비':'diff'
-                ,'시가':'open','고가':'high','저가':'low','거래량':'volume'})
+                ,'시가':'open','고가':'high','저가':'low','거래량':'volume'})    # 한글 칼럼명을 영문으로 변경
             df['date'] = df['date'].replace('.', '-')
             df = df.dropna()
+            # 마리아디비에서 BIGINT형으로 지정한 칼럼들의 데이터형을 int형으로 변경
             df[['close', 'diff', 'open', 'high', 'low', 'volume']] = df[['close',
                 'diff', 'open', 'high', 'low', 'volume']].astype(int)
+            # 칼럼 순서 변경
             df = df[['date', 'open', 'high', 'low', 'close', 'diff', 'volume']]
         except Exception as e:
             print('Exception occured :', str(e))
@@ -113,7 +120,8 @@ class DBUpdater:
     def replace_into_db(self, df, num, code, company):
         """네이버에서 읽어온 주식 시세를 DB에 REPLACE"""
         with self.conn.cursor() as curs:
-            for r in df.itertuples():
+            for r in df.itertuples():   # 데이터프레임을 튜플로 순회처리
+                # REPLACE INTO 구문으로 daily_price 테이블을 업데이트
                 sql = f"REPLACE INTO daily_price VALUES ('{code}', "\
                     f"'{r.date}', {r.open}, {r.high}, {r.low}, {r.close}, "\
                     f"{r.diff}, {r.volume})"
@@ -126,9 +134,11 @@ class DBUpdater:
     def update_daily_price(self, pages_to_fetch):
         """KRX 상장법인의 주식 시세를 네이버로부터 읽어서 DB에 업데이트"""  
         for idx, code in enumerate(self.codes):
+            # read_naver() 메서드를 이용하여 종목코드에 대한 일별 시세 데이터프레임을 구함
             df = self.read_naver(code, self.codes[code], pages_to_fetch)
             if df is None:
                 continue
+            # 일별 시세 데이터프레임이 구해지면 replace_into_db() 메서드로 DB에 저장
             self.replace_into_db(df, idx, code, self.codes[code])            
 
     def execute_daily(self):
@@ -165,5 +175,9 @@ class DBUpdater:
         t.start()
 
 if __name__ == '__main__':
+    # DBUpdaterEx.py가 단독으로 실행되면 DBUpdater 객체를 생성한다.
+    # DBUpdater 생성자 내부에서 마리아디비에 연결한다.
+    # company_info 테이블에 오늘 업데이트된 내용이 있는지 확인하고, 없으면 업데이트
+
     dbu = DBUpdater()
     dbu.execute_daily()
